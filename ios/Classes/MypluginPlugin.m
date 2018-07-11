13b57d7748cf5a904e3b843960e34f92cec4ad6b
600913 <600913@gmail.com>
2018-07-11 14:18:55 +0900

    add merge and save

--- ios/Classes/MypluginPlugin.m
+++ ios/Classes/MypluginPlugin.m
@@ -1,5 +1,7 @@
 #import "MypluginPlugin.h"
 #import <JavaScriptCore/JavaScriptCore.h>
+#import <AVFoundation/AVFoundation.h>
+#import <AssetsLibrary/AssetsLibrary.h>
 
 @implementation MypluginPlugin {
   FlutterResult _flutterResult;
@@ -35,9 +37,58 @@
     JSContext *context = [[JSContext alloc] init];
     NSString *signature = [[context evaluateScript:script] toString];
     result(signature);
+  } else if ([@"mergeAndSave" isEqualToString:call.method]) {
+    NSString *script = call.arguments;
+    JSContext *context = [[JSContext alloc] init];
+    NSString *signature = [[context evaluateScript:script] toString];
+    result(signature);
   } else {
     result(FlutterMethodNotImplemented);
   }
+}
+
+-(void)mergeAndSave:(NSString*)audio_path with:(NSString*)video_path {
+    //Create AVMutableComposition Object which will hold our multiple AVMutableCompositionTrack or we can say it will hold our video and audio files.
+    AVMutableComposition* mixComposition = [AVMutableComposition composition];
+    
+    //Now first load your audio file using AVURLAsset. Make sure you give the correct path of your videos.
+    NSURL *audio_url = [NSURL fileURLWithPath:audio_path];
+    audioAsset = [[AVURLAsset alloc]initWithURL:audio_url options:nil];
+    CMTimeRange audio_timeRange = CMTimeRangeMake(kCMTimeZero, audioAsset.duration);
+    
+    //Now we are creating the first AVMutableCompositionTrack containing our audio and add it to our AVMutableComposition object.
+    AVMutableCompositionTrack *b_compositionAudioTrack = [mixComposition addMutableTrackWithMediaType:AVMediaTypeAudio preferredTrackID:kCMPersistentTrackID_Invalid];
+    [b_compositionAudioTrack insertTimeRange:audio_timeRange ofTrack:[[audioAsset tracksWithMediaType:AVMediaTypeAudio] objectAtIndex:0] atTime:kCMTimeZero error:nil];
+    
+    //Now we will load video file.
+    NSURL *video_url = [NSURL fileURLWithPath:video_path];
+    videoAsset = [[AVURLAsset alloc]initWithURL:video_url options:nil];
+    CMTimeRange video_timeRange = CMTimeRangeMake(kCMTimeZero,audioAsset.duration);
+    
+    //Now we are creating the second AVMutableCompositionTrack containing our video and add it to our AVMutableComposition object.
+    AVMutableCompositionTrack *a_compositionVideoTrack = [mixComposition addMutableTrackWithMediaType:AVMediaTypeVideo preferredTrackID:kCMPersistentTrackID_Invalid];
+    [a_compositionVideoTrack insertTimeRange:video_timeRange ofTrack:[[videoAsset tracksWithMediaType:AVMediaTypeVideo] objectAtIndex:0] atTime:kCMTimeZero error:nil];
+    
+    //decide the path where you want to store the final video created with audio and video merge.
+    NSArray *dirPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *docsDir = [dirPaths objectAtIndex:0];
+    NSString *outputFilePath = [docsDir stringByAppendingPathComponent:[NSString stringWithFormat:@"FinalVideo.mp4"]];
+    NSURL *outputFileUrl = [NSURL fileURLWithPath:outputFilePath];
+    if ([[NSFileManager defaultManager] fileExistsAtPath:outputFilePath])
+        [[NSFileManager defaultManager] removeItemAtPath:outputFilePath error:nil];
+    
+    //Now create an AVAssetExportSession object that will save your final video at specified path.
+    AVAssetExportSession* _assetExport = [[AVAssetExportSession alloc] initWithAsset:mixComposition presetName:AVAssetExportPresetHighestQuality];
+    _assetExport.outputFileType = AVFileTypeMPEG4;
+    _assetExport.outputURL = outputFileUrl;
+    
+    [_assetExport exportAsynchronouslyWithCompletionHandler:
+     ^(void ) {
+         dispatch_async(dispatch_get_main_queue(), ^{
+             [self exportDidFinish:_assetExport];
+         });
+     }
+     ];
 }
 
 - (void)getStreamurl:(NSString*)url {
